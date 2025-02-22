2a16e2ad8a4c1c4b573b58ff66a3d687c3e91892
Muzammil Shah <101082368+Muzammil803@users.noreply.github.com>
2025-02-22 13:19:17 +0500

    analytics

--- voice-over-text-to-speech/src/app/layout.js
+++ voice-over-text-to-speech/src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Viewport } from 'next';
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -115,7 +117,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background text-foreground`}
       >
         {children}
-
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
